fc36473c773dde8cea1c63d1d5590810311f9a2b
CsYBot <67687653+CsYBot@users.noreply.github.com>
2022-02-14 18:19:35 +0300

    Create TR-tr.js

---
+++ language/TR-tr.js
@@ -0,0 +1,278 @@
+{
+  // Embeds
+  "error": "Hata",
+  "info": "Bilgi",
+  "success": "Başarılı",
+  "buy": "Satın Al",
+  "did": "Biliyor musun?",
+  "warn": "UYARI",
+  
+  // Discord Translators
+  "no": "Hayır",
+  "yes": "Evet",
+  "not_found": "Bulunamadı",
+  "role": "Rol",
+  "channel": "Kanal",
+  "time": "Zaman",
+  "action": "Eylem",
+  "conclusion": "Çözüm",
+  "quantity": "Miktar",
+  "success_term": "Başarı",
+  "delete_message": "Mesajı Sil",
+  "logs": "Loglar",
+  "links": "Bağlantılar",
+  "link": "Bağlantı",
+  "go": "Git",
+  "commands": "Komutlar",
+  "uptime_format": "D [Günler], H [Saatler], m [Dakikalar], s [Saniyeler]",
+  "timestramp_format": "Y [Yıllar] D [Günler]",
+  "bot_owner": "Bot Sahibi",
+  "memory_usage": "Hafıza Kullanımı",
+  "operation_time": "Aktiflik Zamanı",
+  "users": "Kullanıcılar",
+  "user": "Kullanıcı",
+  "servers": "Sunucular",
+  "channels": "Kanallar",
+  "version": "Sürüm",
+  "libary": "Kütüphane",
+  "bot_invite": "Bot Daveti",
+  "bot_support_server": "Destek Sunucusu",
+  "bot_join_support_server": "Sunucumuza Katılın",
+  "manage": "Yönet",
+  "invite": "Davet et",
+  "bot_latency": "Bot Gecikmesi",
+  "guild_name": "Sunucu İsmi",
+  "owner": "Sahip",
+  "member_count": "Üye Sayısı",
+  "channels_count": "Kanal Sayısı",
+  "roles": "Roller",
+  "roles_count": "Rol Sayısı",
+  "created_at": "Oluşturulma Tarihi",
+  "status": "Durum",
+  "types": "Türler",
+  "price": "Fiyat",
+  "platforms": "Platformlar",
+  "tags": "Etiketler",
+  "developers": "Geliştiriciler",
+  "publishers": "Paylaşanlar",
+  "win": "Kazandın",
+  "sorry": "Üzgünüm",
+  "joined_server": "Sunucuya Katılma",
+  "joined_discord": "Discord'a Katılma",
+  "commands_no": "Komut Yok!",
+  "actived": "Aktif",
+  "deactived": "Aktif Değil",
+  
+  // Errors
+  "select_box": "Lütfen Aç-Kapat Seçin (open/close) **Şimdi:** ``%var%``",
+  "already_box": "Sistem Davranışı Zaten ``%var%`` Olarak Ayarlandı",
+  "user_none": "Bir Kullanıcı Girmelisiniz",
+  "user_id_none": "Bir Kullanıcı Kimliği Girmelisiniz",
+  "time_none": "Bir Zaman Girmelisiniz",
+  "search_term_none": "Bir Arama Terimi Girmelisiniz",
+  "role_none": "Bir Rol Girmelisiniz",
+  "message_none": "Bir Mesaj Kimliği Girmelisiniz",
+  "quantity_none": "Bir Miktar Girmelisiniz",
+  "channel_none": "Bir Kanal Girmelisiniz",
+  "amount_none": "Bir Tutar Girmelisiniz",
+  "user_valid": "Geçerli Bir Kullanıcı Girmelisiniz",
+  "search_term_valid": "Geçerli Bir Arama Terimi Girmelisiniz",
+  "role_valid": "Geçerli Bir Rol Girmelisiniz",
+  "channel_valid": "Geçerli Bir Kanal Girmelisiniz",
+  "perm_valid": "Üzgünüm, bunu yapamam. Yeterince yetkim yok!",
+  "error_occurred": "Bir hata oluştu!",
+  "double_money": "Kendinize Para Gönderemezsiniz!",
+  "user_error": "Bu Kullanıcıya Para Göndermek Mümkün Değil!",
+  "nsfw_permission_denied": "Kanal nsfw içerik izni reddedildi!",
+  "dm_error": "DM Gönderilemedi! Lütfen Dm Mesajlarına İzin Verin!",
+  "time_out": "Gerekli Sürede Cevap Alınmadı!",
+  "missing_permissions": "Bu Komutu Çalıştırmak için **%var%** İznine ihtiyacınız var",
+  "bot_missing_permissions": "Bu Komutu Çalıştıra Bilmem İçin **%var%** İznine İhtiyacım Var",
+  
+  // Success
+  "success_behavior": "Başarıyla değiştirildi sistem **davranışı:** ``%var%``",
+  "daily_reward": "Başarıyla **(%var%)** Kripto Kazandınız! Yarın Tekrar Gel!",
+  
+  
+  // Advertise
+  "advertise_not_found": "Son Reklam bulunamadı.",
+  
+  // Swear
+  "swear_not_found": "Son küfür bulunamadı.",
+  
+  //Auto Role
+  "autorole_not_active": "Oto Rol Sistemi Aktif Değil!",
+    // * Reseting
+  "autorole_success_close": "Başarıyla, Oto Rol Kapatıldı!",
+  
+    // * Setting
+  "autorole_renewed": "**Başarıyla Yenilenen Oto Rol Sistemi!**",
+  "autorole_opened": "**Başarıyla Açılan Oto Rol Sistemi!**",
+  "autorole_run_desc": "<@%var%>, Rol Başarıyla Verildi.",
+  "autorole_run_title": "<@%var%>, Hoş Geldiniz!",
+  
+    // * Info
+  "autorole_info": "**Oto Rol Sistemi Hakkında Bilgileriniz**",
+  
+  // Ban
+  "ban_success": "Başarıyla (``%var%``) Kullanıcı Yasaklandı!",
+  
+  // Kick
+  "kick_success": "Başarıyla (``%var%``) Kullanıcı Atıldı!",
+  
+  // Clear
+  "clear_max": "Maksimum %var% Mesajını Aynı Anda Silebilirsiniz!",
+  
+  // Coin
+  "coin_format_level_name": "Seviye",
+  "coin_format_crypto_name": "Kripto",
+  "coin_format_money_name": "Coin",
+  "coin_min_give": "(%var%/%var%) Lütfen Daha Fazla Göndermeyi Düşünün!",
+  "coin_no_give": "(%var%/%var%) O kadar paranız yok!",
+  
+  // Timeouts
+  "try_again": "<t:%var%:R> tarihinde tekrar deneyin!",
+  
+  // Fun Commands
+  "gay_success_yep": "Evet, onlar eşcinsel",
+  "gay_success_nope": "Hayır, eşcinsel değiller.",
+  "gay_meter": "🌈 Gay Metre",
+  "gay_percentage": "**Eşcinsel Yüzdesi**",
+  "gay_question": "**Onlar eşcinsel mi?**",
+  
+  // Help
+  "commands_not_found": "Komut Yok!",
+  "commands_legendary": "Efsanevi Komutlar",
+  "commands_premium": "Premium Komutlar",
+  "commands_utilty": "Komutlar",
+  "commands_fun": "Eğlence Komutlar",
+  "commands_moderation": "Moderasyon Komutları",
+  
+  // Invite
+  "invite_text_title": "<a:tac:795632061406642176> %var% Sunucuya Ekle <a:tac:795632061406642176>",
+  "invite_text_description": "Bizimle Daima İleri!\n\n**Muhteşem Özelliklerle CsYBot Eklemek ister misiniz?** \n\n Bizi Desteklemek ve Harika Özellikler Kazanmak İçin [Tıklayın](%var%)",
+  
+  // Premium
+  "premium_none": "Bir Premium Kod Girmelisiniz",
+  "premium_wrong": "Bu Premium Kod Yanlış Veya Daha Önce Hiç Olmadı!",
+  "premium_valid": "Bu Komut Premium Sunucular İçin Geçerlidir. **Satın al:** ``/premium``",
+  "premium_expire": "Premium Kalan Son Süresi: <t:%var%:R>",
+  "premium_this_yes": "Evet Bu Sunucu Premium! Kalan Süreyi Öğrenmek İçin ``/premium-times``",
+  "premium_this_no": "Hayır Bu Sunucu Premium Değil!",
+  "premium_buy": `**<a:tac:795632061406642176> Bilgiler <a:tac:795632061406642176>**
+\nMerhaba Premium Size Sunucunuzu Büyülüyecek
+Harika Özellikler Sağlayacak Premium **1$**\n
+**Nasıl Satın Alırım?**
+Satın almak için: %var%
+\n**Premium Kodunuz Var mı?**\nÇok Güzel Şimdi Kullanın: \`\`/premium-code <code>\`\`\n\n**Peki Bu Sunucu Premium mu?**
+%var%`,
+  "premium_buy_desc": "Teşekkürler, Sunucunuz Artık Premium! **\"__Şimdi İleri Daha Yakın!__\"**",
+
+  // Reaction
+  "react_already": "Bu kimliğe sahip bir Emoji Rol zaten var.",
+  "react_max": "(%var%/%var%) Emoji Rol Limiti Aşıldı!",
+  "react_success": "**Başarıyla Emoji Rol Sistemi Eklendi!**",
+  "react_not_found": "Emoji Rol bulunamadı.",
+  "react_deleted": "**Başarıyla Silindi (%var%)**",
+  
+  // Security
+  "security_not_active": "Güvenlik Sistemi Aktif Değil!",
+  "security_success_closed": "Başarıyla, Güvenlik Kapatıldı!",
+  "security_none_kickable": "Bir Atılma Girmelisiniz",
+  "security_success_renew": "**Güvenlik Sistemi Başarıyla Yenilendi!**",
+  "security_success_opened": "**Güvenlik Sistemi Başarıyla Açıldı!**",
+  "security_kick_system": "Atılma Sistemi",
+  "security_info": "**Güvenlik Sistemi Hakkındaki Bilgileriniz**",
+  "security_run_status": "Güvenlik Durumu",
+  "security_run_kick": "Atıldı",
+  "security_run_safe": "Kullanıcı Güvenli",
+  "security_run_unsafe": "Kullanıcı Güvenli Değil",
+  "security_run_account_date": "Hesap Oluşturma Tarihi",
+  "security_run_title": "<@%var%> Kullanıcı Katıldı",
+  "security_run_kick_yes": "Evet, Atıldı!",
+  "security_run_kick_no": "Hayır, Atılmadı!",
+  "security_kickable_info": "Yeni Hesaplar Atılıcaktır!",
+  
+  // Shard
+  "shard_title": "👨‍💻 Bot Shardlar (%var%)",
+  
+  // Shop
+  "shop_command_title": "**Mağaza Komutları;**",
+  "shop_items": "**Mağaza Öğeleri;**",
+  "shop_item_not_found": "Ürün Artık Satılık Değil Veya Böyle Bir Şey Hiç Olmadı!",
+  "shop_price_no": "**(__%var%/%var%__)** **(__%var%__)** Ürününüzü Almak İçin Yeterli Paranız Yok!",
+  "shop_waiting": "``%var% - %var%`` Ürünü satın almak istediğinizden emin misiniz?\n **Not:** __Asla İade Edemezsiniz!__",
+  "shop_result_code": `**(__%var%__)** Ürününü Başarıyla Satın Aldınız.
+      **__İşte Premium Kodunuz:__** \`\`%var%\`\``,
+  "shop_result_template": `**(__%var%__)** Ürününü Başarıyla Satın Aldınız.
+      **__İşte Gizli Kodunuz:__** \`\`%var%\`\`
+      **__İşte İndirme Bağlantınız:__** [bağlantı](%var%)`,
+  "shop_purchased": "**(__%var%__)** Ürün Başarıyla Satın Alındı!",
+  "shop_rejected": "Başarıyla Reddedildi, İstediğiniz Zaman Satın Alabilirsiniz!",
+  "shop_list": `%var%\n**Nasıl Satın Alırım?**\nAlacağınız Ürünün Kimliğini Bilmelisiniz Sonra Yazmanız Yeterli\`\`/shop <item-id>\`\`!`,
+
+  // Steam
+  "steam_title": "Steam Mağazası",
+  "steam_game_id": "Oyun Kimliği",
+  "steam_regular": "Normal fiyat",
+  "steam_discounted": "İndirimli fiyat",
+  
+  // Steel Safe
+  "steel_safe_bet_none": "Ne Kadar Bahis Yapacaksınız?",
+  "steel_safe_not_bet": "(%var%/%var%) Yeterli paranız yok!",
+  "steel_safe_max_bet": "(%var%/%var%) Çok Büyük Oynayamazsınız",
+  "steel_safe_waiting": "Destek Sunucumuzda Ses Kanallarına Katılarak +%35 Şans Elde Edebilirsiniz! (**__/contact__**)..",
+
+  // Temp Ban
+  "ban_temp_max": "Üzgünüz, maksimum 1 hafta Süreli yasaklayabilirsiniz!",
+  "ban_temp_success": "Başarıyla (``%var%``) Kullanıcı Süreli Banlandı, Açılacağı zaman: <t:%var%:R>!",
+  
+  //Un Ban
+  "unban_success": "Başarıyla (``%var%``) Kullanıcı Yasağı Kaldırıldı!",
+  
+  // Un Mute
+  "unmute_success": "Başarıyla (``%var%``) Kullanıcının Susturulması Açıldı!",
+  
+  // Vote
+  "vote_title": "Oylarınız Bize Yardımcı Olacak!",
+  "vote_desc": "Her Oy Verdiğinizde (12 Saat) Size Güncel (%var%) Madeni Para Verilecektir! [Oy](%var%)",
+
+  // Spam
+  "spam_success_muted": "Çok Fazla Mesaj Gönderdiniz. Kanaldan Yasaklandınız!",
+  "spam_warning": "Çok Fazla Mesaj Gönderdiniz. Lütfen Bunu Yapmayı Durdurun!",
+  
+  // Brain
+  "brain_null": "Lütfen Bir Soru Belirtin!",
+  "brain_perm_valid": "Bu Komut Sunucu Yöneticisi Tarafından Önce Kullanılmalıdır!",
+  "brain_term_info": "Öncelikle bu sistem sadece Türkçe sonuçları ve Türkçe soruları almalıdır.\nTürkçe Konuştuğunuzu %100 kabul etmelisiniz.\n **Not:** __Sonuçlardan asla sorumlu tutulamayız!__",
+  "brain_success_opened": "Başarıyla Etkinleştirildi! CsYBot Yapay Zekasını Kullanabilirsiniz.",
+  "brain_success_reject": "Başarıyla Reddedildi, İstediğiniz Zaman Açabilirsiniz!",
+  
+  // Main Protector
+  "protect_message": "Bu Sunucu CsYBot Tarafından Korunmaktadır!",
+  
+  // Term Policies
+  "term_policies_desc": `Merhaba <@%var%>,
+CsYBot'u kullanmak için kuralları kabul etmelisiniz.
+
+↳ Bottaki bir hata bulduğunuzda geliştiricilerle __**%var%**__ aracılığıyla iletişime geçin.
+↳ Bot'a ping atmak için komutları kullanmayın.
+↳ Bot benzeri & aynı botu açmayın, altyapısını oluşturmayın Paylaşmayın.
+↳ Bot ile yaptığınız illegal (trojen, token grabber, vb.) dosyaların sorumluluğu hesaba aittir.
+↳ Discord Topluluk Kurallarına dikkat edin.
+↳ Kabul Eden Kişi Sayısı: (\`\`%var%\`\`)
+
+↳ Veri saklama politikamız;
+Tüm bilgiler sitemizde yer almaktadır.`,
+  
+  // Captcha
+  "captcha_wait": "Resimde Ne Görüyorsun?",
+  "captcha_error": "İnsan doğrulaması başarısız oldu",
+ 
+  // Level
+  "level_max": "You have reached the max Level. (%var%)",
+  
+  // Code
+  "code_download": "Aşağıdan indirip kullanabilirsiniz",
+  "code_list": `%var%\n\n**Nasıl Kodları İndiririm?**\nAlacağınız Kodun Kimliğini Bilmelisiniz Sonra Yazmanız Yeterli \`\`/code get <code-id>\`\`!`,
+}
