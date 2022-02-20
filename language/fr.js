470aadd3e8d85f2975451991c1f9a47c88f271de
CsYBot <67687653+CsYBot@users.noreply.github.com>
2022-02-20 16:34:10 +0300

    Create fr.js

---
+++ language/fr.js
@@ -0,0 +1,285 @@
+module.exports = {
+  // Embeds
+  "error": "Erreur",
+  "info": "Info",
+  "success": "Réussi",
+  "buy": "ACHETER",
+  "did": "Le saviez-vous?",
+  "warn": "ATTENTION",
+  
+  // Discord Translators
+  "no": "Non",
+  "yes": "Oui",
+  "not_found": "Pas trouvé",
+  "role": "Rôle",
+  "channel": "Canal",
+  "time": "Temps",
+  "action": "Action",
+  "conclusion": "Conclusion",
+  "quantity": "Quantity",
+  "success_term": "Succès",
+  "delete_message": "Supprimer le message",
+  "logs": "Journaux",
+  "links": "Liens",
+  "link": "Lien",
+  "go": "va",
+  "commands": "Commandes",
+  "uptime_format": "D [Journées], H [Les heures], m [Minutes], s [Seconds]",
+  "timestramp_format": "Y [Ans] D [Journées]",
+  "bot_owner": "Propriétaire du robot",
+  "memory_usage": "Utilisation de la mémoire",
+  "operation_time": "Moment de l'opération",
+  "users": "Utilisateurs",
+  "user": "Utilisateur",
+  "servers": "Les serveurs",
+  "channels": "Chaînes",
+  "version": "Version",
+  "libary": "Bibliothèque",
+  "bot_invite": "Inviter un robot",
+  "bot_support_server": "Serveur d'assistance",
+  "bot_join_support_server": "Rejoignez notre serveur",
+  "manage": "Faire en sorte",
+  "invite": "Inviter",
+  "bot_latency": "Latence du robot",
+  "guild_name": "Nom de guilde",
+  "owner": "Propriétaire",
+  "member_count": "Nombre de membres",
+  "channels_count": "Nombre de chaînes",
+  "roles": "Les rôles",
+  "roles_count": "Nombre de rôles",
+  "Roles": "Nombre de rôles",
+  "created_at": "Créé à",
+  "status": "Statut",
+  "types": "Les types",
+  "price": "Prix",
+  "platforms": "Plateformes",
+  "tags": "Mots clés",
+  "developers": "Développeurs",
+  "publishers": "Éditeurs",
+  "win": "Gagner",
+  "sorry": "Pardon",
+  "joined_server": "Serveur joint",
+  "joined_discord": "Discord rejoint",
+  "commands_no": "Aucune commande !",
+  "actived": "Activé",
+  "deactived": "Désactivé",
+  
+  // Errors
+  "select_box": "Veuillez Ouvrir-Fermer Sélectionner (ouvrir/fermer)**Maintenant:** ``%var%``",
+  "already_box": "Comportement du système déjà défini sur ``%var%``",
+  "user_none": "Vous devez entrer un utilisateur",
+  "user_id_none": "Vous devez saisir un ID utilisateur",
+  "time_none": "Vous devez saisir une heure",
+  "search_term_none": "Vous devez entrer un terme de recherche",
+  "role_none": "Vous devez entrer un rôle",
+  "message_none": "Vous devez entrer un ID de message",
+  "quantity_none": "Vous devez entrer une quantité",
+  "channel_none": "Vous devez entrer un canal",
+  "amount_none": "Vous devez entrer un montant",
+  "user_valid": "Vous devez entrer un utilisateur valide",
+  "search_term_valid": "Vous devez entrer un terme de recherche valide",
+  "role_valid": "Vous devez entrer un rôle valide",
+  "channel_valid": "Vous devez entrer un canal valide",
+  "perm_valid": "Désolé, je ne peux pas faire ça. Je n'ai pas assez d'autorité !",
+  "error_occurred": "Une erreur s'est produite !",
+  "double_money": "Vous ne pouvez pas vous envoyer d'argent !",
+  "user_error": "Il n'est pas possible d'envoyer de l'argent à cet utilisateur !",
+  "nsfw_permission_denied": "Autorisation de contenu de la chaîne nsfw refusée !",
+  "dm_error": "Échec de l'envoi du DM ! Veuillez autoriser les messages Dm !",
+  "time_out": "Aucune réponse reçue dans les délais requis !",
+  "missing_permissions": "Vous avez besoin de l'autorisation **%var%** pour exécuter cette commande",
+  "bot_missing_permissions": "J'ai besoin de l'autorisation **%var%** pour exécuter cette commande",
+  
+  // Success
+  "success_behavior": "Système **comportement :** ``%var%`` modifié avec succès",
+  "daily_reward": "
+**(%var%)** Crypto que vous avez gagné ! Revenez demain !",
+  
+  
+  // Advertise
+  "advertise_not_found": "
+La dernière annonce n'a pas été trouvée.",
+  
+  // Swear
+  "swear_not_found": "Le dernier gros mot n'a pas été trouvé.
+",
+  
+  //Auto Role
+  "autorole_not_active": "Le système AutoRole n'est pas actif !
+",
+    // * Reseting
+  "autorole_success_close": "Avec succès, AutoRole fermé !",
+  
+    // * Setting
+  "autorole_renewed": "**Système AutoRole renouvelé avec succès !**",
+  "autorole_opened": "**Système AutoRole ouvert avec succès !**",
+  "autorole_run_desc": "<@%var%>, Rôle attribué avec succès.",
+  "autorole_run_title": "<@%var%>, Bienvenu!",
+  
+    // * Info
+  "autorole_info": "**Vos informations sur le système AutoRole**",
+  
+  // Ban
+  "ban_success": "Utilisateur banni avec succès (``%var%``) !",
+  
+  // Kick
+  "kick_success": "L'utilisateur a été expulsé avec succès (``%var%``) !",
+  
+  // Clear
+  "clear_max": "Vous pouvez supprimer un maximum de %var% de messages en même temps !",
+  
+  // Coin
+  "coin_format_level_name": "Niveau",
+  "coin_format_crypto_name": "Crypto",
+  "coin_format_money_name": "Pièce de monnaie",
+  "coin_min_give": "(%var%/%var%) S'il vous plaît, envisagez d'en publier plus !",
+  "coin_no_give": "(%var%/%var%) Vous n'avez pas beaucoup d'argent !",
+  
+  // Timeouts
+  "try_again": "Réessayez sur <t:%var%:R>!",
+  
+  // Fun Commands
+  "gay_success_yep": "Oui, ils sont gays",
+  "gay_success_nope": "Non, ils ne sont pas gay.",
+  "gay_meter": "🌈 Gay Mètre",
+  "gay_percentage": "**Pourcentage d'gay**",
+  "gay_question": "**Sont-ils gay ?**",
+  
+  // Help
+  "commands_not_found": "Aucune commande !",
+  "commands_legendary": "Commandes légendaires",
+  "commands_premium": "Commandes premium",
+  "commands_utilty": "Commandes",
+  "commands_fun": "Commandes amusantes",
+  "commands_moderation": "Commandes de modération",
+  
+  // Invite
+  "invite_text_title": "<a:tac:795632061406642176> %var% Ajout au serveur <a:tac:795632061406642176>",
+  "invite_text_description": "Toujours en avant avec nous ! \n\n**CsYBot avec d'excellentes fonctionnalités Souhaitez-vous l'ajouter ?** \n\nPour nous soutenir et bénéficier de fonctionnalités intéressantes [Cliquez](%var%)",
+  
+  // Premium
+  "premium_none": "Vous devez entrer un code premium",
+  "premium_wrong": "Ce code premium est erroné ou n'a jamais existé auparavant !",
+  "premium_valid": "Cette commande est valide pour les serveurs Premium. **Acheter :** ``/premium``",
+  "premium_expire": "Dernière expiration restante de la prime : <t:%var%:R>",
+  "premium_this_yes": "Oui, ce serveur est Premium ! Pour connaître le temps restant ``/premium-times``",
+  "premium_this_no": "Non, ce serveur n'est pas Premium !",
+  "premium_buy": `**<a:tac:795632061406642176> Informations <a:tac:795632061406642176>**
+\nHello Premium System Votre serveur est Premium Taille Premium
+Fournira des fonctionnalités intéressantes La prime est de **1$**
+\n
+**Comment acheter ?**
+Pour acheter: %var%
+\n**Vous avez un code premium ?**\nC'est tellement beau Utilisez-le maintenant: \`\`/premium-code <code>\`\`\n\n**Alors, est-ce que ce serveur est Premium ?** 
+%var%`,
+  "premium_buy_desc": "Merci, votre serveur est maintenant Premium ! **\"__Maintenant, l'avant est plus proche !__\"**",
+
+  // Reaction
+  "react_already": "Un React avec cette identité existe déjà.",
+  "react_max": "(%var%/%var%) Limite de réaction dépassée!",
+  "react_success": "**Système de rôle de réaction ajouté avec succès !**",
+  "react_not_found": "Aucune réaction trouvée.",
+  "react_deleted": "**Supprimé avec succès (%var%)**",
+  
+  // Security
+  "security_not_active": "Le système de sécurité n'est pas actif !",
+  "security_success_closed": "Avec succès, sécurité fermée !",
+  "security_none_kickable": "Vous devez entrer un Kickable",
+  "security_success_renew": "**Système de sécurité renouvelé avec succès !**",
+  "security_success_opened": "**Système de sécurité ouvert avec succès !**",
+  "security_kick_system": "Système de coup de pied",
+  "security_info": "**Vos informations sur le système de sécurité**",
+  "security_run_status": "État de la sécurité",
+  "security_run_kick": "Ont été frappés",
+  "security_run_safe": "Coffre-fort utilisateur",
+  "security_run_unsafe": "Utilisateur non sécurisé !",
+  "security_run_account_date": "Date de création du compte",
+  "security_run_title": "<@%var%> Utilisateur rejoint",
+  "security_run_kick_yes": "Oui, Kicked!",
+  "security_run_kick_no": "Non, pas frappé !",
+  "security_kickable_info": "Les nouveaux comptes seront supprimés !",
+  
+  // Shard
+  "shard_title": "👨‍💻 Fragments de bot (%var%)",
+  
+  // Shop
+  "shop_command_title": "**Commandes de la boutique;**",
+  "shop_items": "**Articles de la boutique;**",
+  "shop_item_not_found": "L'article n'est plus à vendre ou quelque chose comme ça n'a jamais existé !",
+  "shop_price_no": "**(__%var%/%var%__)** **(__%var%__)** Vous n'avez pas assez d'argent pour acheter votre article !",
+  "shop_waiting": "``%var% - %var%`` Êtes-vous sûr de vouloir acheter le produit ?\n **Noter:** __Vous ne pourrez jamais être remboursé !__",
+  "shop_result_code": `Vous avez acheté avec succès le produit **(__%var%__)**
+      **__Voici votre code Premium:__** \`\`%var%\`\``,
+  "shop_result_template": `Vous avez acheté avec succès le produit **(__%var%__)**.
+      **__Voici votre code secret:__** \`\`%var%\`\`
+      **__Voici votre lien de téléchargement:__** [lien](%var%)`,
+  "shop_purchased": "**(__%var%__)** Article acheté avec succès!",
+  "shop_rejected": "Rejeté avec succès, vous pouvez l'acheter à tout moment!",
+  "shop_list": `%var%\n**Comment puis-je acheter ?**\nVous devez connaître le nom du produit que vous allez acheter, puis écrivez simplement \`\`/shop <item-id> \`\`!`,
+  "shop_level_min": "**(__%var%/%var%__)** **(__%var%__)** Vous n'avez pas assez de niveau pour acheter votre article !",
+
+  // Steam
+  "steam_title": "magasin de vapeur",
+  "steam_game_id": "Identifiant de jeu",
+  "steam_regular": "Prix habituel",
+  "steam_discounted": "Prix réduit",
+  
+  // Steel Safe
+  "steel_safe_bet_none": "Combien allez-vous parier ?",
+  "steel_safe_not_bet": "(%var%/%var%) Vous n'avez pas assez d'argent !",
+  "steel_safe_max_bet": "(%var%/%var%) Vous ne pouvez pas jouer trop gros",
+  "steel_safe_waiting": "Vous pouvez obtenir +35 % de chance en rejoignant les canaux vocaux sur notre serveur d'assistance ! (**__/contact__**)..",
+
+  // Temp Ban
+  "ban_temp_max": "Désolé, vous pouvez bannir 1 semaine max !",
+  "ban_temp_success": "Avec succès (``%var%``) User TempBanned, Quand il s'ouvrira : <t:%var%:R> !",
+  
+  //Un Ban
+  "unban_success": "Suppression réussie du bannissement utilisateur (``%var%``) !",
+  
+  // Un Mute
+  "unmute_success": "Réussite (``%var%``) Utilisateur désactivé !",
+  
+  // Vote
+  "vote_title": "Votre vote nous aidera !",
+  "vote_desc": "Chaque fois que vous votez (12 heures), vous recevrez des pièces actuelles (%var%) ! [Voter](%var%)",
+
+  // Spam
+  "spam_success_muted": "Vous avez envoyé trop de messages. Vous avez été banni de la chaîne !",
+  "spam_warning": "Vous avez envoyé trop de messages. S'il vous plaît, arrêtez de faire ça !",
+  
+  // Brain
+  "brain_null": "Veuillez poser une question !",
+  "brain_perm_valid": "Cette commande ne peut pas être utilisée sans activation par un administrateur de serveur !",
+  "brain_term_info": "Tout d'abord, ce système est produit en anglais. Il peut y avoir des erreurs de traduction.\nVous pouvez nous contacter si vous pensez qu'il y a une erreur.\n\n**Remarque :** __Nous ne pouvons jamais être tenus responsables des résultats !__",
+  "brain_success_opened": "Activé avec succès ! Vous pouvez utiliser l'intelligence artificielle CsYBot.",
+  "brain_success_reject": "Rejeté avec succès, vous pouvez ouvrir à tout moment !",
+  
+  // Main Protector
+  "protect_message": "Ce serveur est protégé par CsYBot!",
+  
+  // Term Policies
+  "term_policies_desc": `Bonjour <@%var%>,
+Vous devez accepter les règles d'utilisation de CsYBot
+
+↳ Contactez les développeurs via __**%var%**__ dans un bogue de recherche sur le bot.
+↳ N'utilisez pas de commandes pour envoyer un ping au bot.
+↳ N'ouvrez pas un bot-like & same bot, ne créez pas son infrastructure.
+↳ La responsabilité des fichiers illégaux (trojen, token grabber, etc.) que vous créez avec le bot appartient au compte.
+↳ Faites attention aux directives de la communauté Discord.
+↳ Nombre de personnes acceptant : (\`\`%var%\`\`)
+
+↳ Notre politique de stockage des données ;
+Toutes les informations sont sur notre site.`,
+  
+  // Captcha
+  "captcha_wait": "Que voyez-vous dans la photo?",
+  "captcha_error": "La vérification humaine a échoué",
+  
+  // Level
+  "level_max": "Vous avez atteint le niveau maximum. (%var%)",
+  
+  // Code
+  "code_download": "Vous pouvez télécharger et utiliser ci-dessous",
+  "code_list": `%var%\n\n**Comment télécharger des codes ? **\nAprès avoir connu l'ID du code que vous obtiendrez, il suffit de l'écrire \`\`/code get <code-id>\`\`!`
+}
