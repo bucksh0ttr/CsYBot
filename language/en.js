6b797855d988288761f3c7ef652debc208631047
CsYBot <67687653+CsYBot@users.noreply.github.com>
2022-02-26 16:36:34 +0300

    Update en.js

--- language/en.js
+++ language/en.js
@@ -249,6 +249,7 @@ For buy: %var%
   "brain_term_info": "First of all, this system is produced in English. There may be translation errors.\nYou can contact us if you think there is an error.\n\n**Note:** __We can never be held responsible for the results!__",
   "brain_success_opened": "Activated Successfully! You Can Use CsYBot Artificial Intelligence.",
   "brain_success_reject": "Successfully Rejected, You Can Open Anytime!",
+  "brain_problem": "I spoke to 3,000 people this week. I am tired.",
   
   // Main Protector
   "protect_message": "This Server is Protected by CsYBot!",
